07037aac852333d50f20ba4190993f94574e06bb
Allan Nava <allannava95@gmail.com>
2024-04-17 10:05:40 +0200

    removed cat

--- build.sh
+++ build.sh
@@ -44,11 +44,11 @@ git clone --depth 1 https://github.com/Haivision/srt.git
 mkdir -p ~/srt/build
 cd ~/srt/
 ls -l 
-cat CMakeLists.txt
+#cat CMakeLists.txt
 #
 pwd
 #
-#cmake -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_C_DEPS=ON -DENABLE_SHARED=ON ..
+cmake -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_C_DEPS=ON -DENABLE_SHARED=ON ..
 make
 make install
 #
