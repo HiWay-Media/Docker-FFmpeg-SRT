ab53b025ba525d4bfee2d98205085c723f4ac653
Reza <49395861+iesreza@users.noreply.github.com>
2024-04-17 10:19:23 +0200

    fix cmake path

--- build.sh
+++ build.sh
@@ -42,13 +42,13 @@ pwd
 git clone --depth 1 https://github.com/Haivision/srt.git
 #
 mkdir -p ~/srt/build
-cd ~/srt/
+cd ~/srt/build
 ls -l 
 #cat CMakeLists.txt
 #
 pwd
 #
-cmake -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_C_DEPS=ON -DENABLE_SHARED=ON .
+cmake -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_C_DEPS=ON -DENABLE_SHARED=ON ..
 make
 make install
 #
